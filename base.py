99c53648dabec8db92ed6eed84fda276e143a767
ntdens <40859491+ntdens@users.noreply.github.com>
2018-07-05 10:36:29 -0400

    Add files via upload

--- base.py
+++ base.py
@@ -1,5 +1,14 @@
 import pandas as pd
+import plotly
+import plotly.offline as py
+import cufflinks as cf
+import plotly.graph_objs as go
+import ipywidgets as widgets
+import numpy as np
 
-df = pd.read_csv('CME-FCK2019.csv')
-df.set_index('Date', inplace=True)
-print(df)
+stocks = pd.read_csv('CME-FCK2019.csv')
+print(stocks)
+py.plot({
+    "data": [go.Scatter(x=stocks.Date, y=stocks.Change)],
+    "layout": go.Layout(title="Change Over Time")
+}, auto_open=True)
