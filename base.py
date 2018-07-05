4d73dd020012a2426691caf7103f0c16b46495b2
ntdens <40859491+ntdens@users.noreply.github.com>
2018-07-05 11:48:05 -0400

    Add files via upload
    Now prettier

--- base.py
+++ base.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import plotly
 import plotly.offline as py
-import cufflinks as cf
 import plotly.graph_objs as go
-import ipywidgets as widgets
-import numpy as np
 
 stocks = pd.read_csv('CME-FCK2019.csv')
-print(stocks)
+stocks.set_index('Date', inplace=True)
+price = stocks.drop(columns=['Change','Volume','Previous Day Open Interest'])
+print(price)
 py.plot({
-    "data": [go.Scatter(x=stocks.Date, y=stocks.Change)],
+    "data": [go.Scatter(
+        x=price.index,
+        y=price[col],
+        name= col)
+        for col in price.columns],
     "layout": go.Layout(title="Change Over Time")
 }, auto_open=True)
+
