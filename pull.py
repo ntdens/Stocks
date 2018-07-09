6b13cb28690ee274c1baa0d105afe46b98f12df9
ntdens <ntdens@gmail.com>
2018-07-09 09:38:50 -0400

    Added mode functionality

--- pull.py
+++ pull.py
@@ -1,51 +1,82 @@
-import pandas as pd
-import requests
-import plotly.offline as py
-import plotly.graph_objs as go
-import io
 import argparse as ag
 from datetime import date, timedelta
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+
 
 def main():
     parser = ag.ArgumentParser(prog='pull', description='Pulls stock data from AlphaVantage.co')
-    parser.add_argument('--stocks',nargs='*',help='Enter a list of stock tickers',metavar='XXX',default=['T','ATVI','MSFT'])
-    parser.add_argument('--start-date',help='Enter a starting date in the YYYY-MM-DD format',dest='start')
+    parser.add_argument('--stocks', nargs='*', help='Enter a list of stock tickers', metavar='XXX',
+                        default=['T', 'ATVI', 'MSFT'])
+    parser.add_argument('--start-date', help='Enter a starting date in the YYYY-MM-DD format', dest='start')
     parser.add_argument('--end-date', help='Enter a ending date in the YYYY-MM-DD format', dest='end')
     mode = parser.add_mutually_exclusive_group()
-    mode.add_argument('--mode-high', help='Prints out the highest price reached during the period')
-    mode.add_argument('--mode-low', help='Prints out the lowest price reached during the period')
-    mode.add_argument('--mode-last',help='Prints out the most recent price')
-    mode.add_argument('--mode-earliest', help='Prints out the earliest date')
-    mode.add_argument('--mode-change',help='print out the change between the last and the earliest')
-    mode.add_argument('--mode-all', help='Prints all of the above')
+    mode.add_argument('--mode-high', action='store_true', help='Prints out the highest price reached during the period')
+    mode.add_argument('--mode-low', action='store_true', help='Prints out the lowest price reached during the period')
+    mode.add_argument('--mode-last', action='store_true', help='Prints out the most recent price')
+    mode.add_argument('--mode-earliest', action='store_true', help='Prints out the earliest date')
+    mode.add_argument('--mode-change', action='store_true',
+                      help='Prints out the change between the last and the earliest')
+    mode.add_argument('--mode-all', action='store_true', help='Prints all of the above')
     args = parser.parse_args()
     index = args.stocks
     start_date = args.start
     if not start_date:
-       start_date = date.today() - timedelta(days=1)
-       start_date = start_date.strftime('%Y-%m-%d')
+        start_date = date.today() - timedelta(days=1)
+        start_date = start_date.strftime('%Y-%m-%d')
     end_date = args.end
     if not end_date:
-       end_date = date.today()
-       end_date = end_date.strftime('%Y-%m-%d')
-
+        end_date = date.today()
+        end_date = end_date.strftime('%Y-%m-%d')
 
     stocks = pd.DataFrame()
     for ticker in index:
-       r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&datatype=csv&apikey=603MDIJGG9TGNV60'.format(ticker)).content
-       df = pd.read_csv(io.StringIO(r.decode()))
-       df.set_index('timestamp', inplace=True)
-       df = df.drop(['high', 'low', 'close', 'volume'], axis=1)
-       df = df.rename(columns={'open': ticker})
-       if stocks.empty:
-           stocks = df
-       else:
-           stocks = stocks.join(df)
+        ts = TimeSeries(key='603MDIJGG9TGNV60', output_format='pandas')
+        data, metadata = ts.get_daily(symbol='{}'.format(ticker))
+        df = data
+        df = df.drop(['2. high', '3. low', '4. close', '5. volume'], axis=1)
+        df = df.rename(columns={'1. open': ticker})
+        if stocks.empty:
+            stocks = df
+        else:
+            stocks = stocks.join(df)
 
+    stocks = stocks.loc[start_date:end_date]
+    print(stocks)
 
-    stocks.index = pd.to_datetime(stocks.index)
-    print (stocks.loc[end_date:start_date])
+    if args.mode_high:
+        print('Peak Values:')
+        print(stocks.max())
 
+    if args.mode_low:
+        print('Lowest Values:')
+        print(stocks.min())
+
+    if args.mode_last:
+        print('Latest Values:')
+        print(stocks.iloc[0])
+
+    if args.mode_earliest:
+        print('Earliest Values:')
+        print(stocks.iloc[-1])
+
+    if args.mode_change:
+        print('Change in Values:')
+        stocks.loc['Change'] = stocks.iloc[0] - stocks.iloc[-1]
+        print (stocks.loc['Change'])
+
+    if args.mode_all:
+        print('Peak Values:')
+        print(stocks.max())
+        print('Lowest Values:')
+        print(stocks.min())
+        print('Latest Values:')
+        print(stocks.iloc[0])
+        print('Earliest Values:')
+        print(stocks.iloc[-1])
+        print('Change in Values:')
+        stocks.loc['Change'] = stocks.iloc[0] - stocks.iloc[-1]
+        print (stocks.loc['Change'])
 
 #    py.plot({
 #        "data": [go.Scatter(
@@ -56,7 +87,6 @@ def main():
 #        "layout": go.Layout(title="Daily Openings")
 #    }, auto_open=True)
 
+
 if __name__ == '__main__':
     main()
-
-
